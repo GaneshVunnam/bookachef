2ec1923f6e256b5858388acfed557ed4f07a604c
Ganesh <ganeshvunnam8@gmail.com>
2024-03-30 14:07:57 +0530

    db added

--- db/bookachef.sql
+++ db/bookachef.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 06, 2024 at 11:30 AM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- Generation Time: Mar 30, 2024 at 09:36 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -55,9 +55,12 @@ CREATE TABLE `tbl_bookings` (
 
 INSERT INTO `tbl_bookings` (`id`, `user`, `city`, `guest`, `date`, `meal`, `cuisine`, `ingredients`, `restrictions`, `conditions`, `dishes`, `request`, `direct_request`, `status`, `payment`, `amount`, `chef_update`, `payment_id`, `created_on`) VALUES
 (2, 3, 'Chennai', 3, '2024-02-14', 'Dinner', '14', 'Chef', 'Dairy', 'I don\\\'t like gee', '1,2,3,4', 'Open', '2', 'Accepted', 'NA', 0, 0, '', '2024-02-02 03:15:50'),
-(3, 3, 'Mumbai', 3, '2024-02-29', 'Lunch', '14', 'Customer', 'Shellfish,Dairy', 'I dont like mutton', '2,3,4', 'Open', '2', 'Paid', 'YES', 1451, 1, 'pay_NXRduVx9bBLpjt', '2024-02-02 05:07:45'),
-(4, 3, 'Chennai', 14, '2024-03-19', 'Dinner', '14', '', 'Vegetarian', 'I like Chicken', '1,2,3,4', 'Open', '2', 'Paid', 'YES', 9664, 0, 'pay_NXYoiMVpZ0grMP', '2024-02-05 11:27:59'),
-(10, 3, 'chennai', 5, '2024-02-07', 'Breakfast', '14', '', '', 'nvbvvbvbvfhdid', '3', 'Open', 'NA', 'Order Received', 'NA', 0, 0, '', '2024-02-06 10:43:02');
+(12, 4, 'Kandukur', 2, '2024-03-25', 'Breakfast', '14', '', '', '', '5,7,10,16,18', 'Open', '2', 'Paid', 'YES', 1628, 0, 'pay_NlLOX6sPOeg5FF', '2024-03-12 07:24:25'),
+(13, 4, 'Kandukur', 2, '2024-03-25', 'Lunch', '14', '', '', '', '5,7,16', 'Open', '2', 'Paid', 'YES', 684, 0, 'pay_NlNAnj8k8sFbaN', '2024-03-12 09:07:40'),
+(14, 4, 'chennai', 2, '2024-03-21', 'Dinner', '14', '', '', '', '16,17', 'Open', '2', 'Accepted', 'YES', 472, 0, '', '2024-03-12 09:18:06'),
+(15, 4, 'chennai', 2, '2024-03-27', 'Breakfast', '14', '', '', '', '1,7,14', 'Open', '2', 'Accepted', 'YES', 1499, 0, '', '2024-03-12 09:39:01'),
+(25, 4, 'chennai', 2, '2024-04-02', 'Lunch', '14', '', '', '', '10,16', 'Open', '2', 'Accepted', 'YES', 944, 0, '', '2024-03-30 07:43:50'),
+(26, 4, 'chennai', 2, '2024-04-02', 'Lunch', '14', '', '', '', '15,16', 'Open', '2', 'Accepted', 'YES', 1062, 0, '', '2024-03-30 07:54:36');
 
 -- --------------------------------------------------------
 
@@ -76,7 +79,10 @@ CREATE TABLE `tbl_category` (
 
 INSERT INTO `tbl_category` (`id`, `name`) VALUES
 (1, 'Sandwich'),
-(2, 'Deserts');
+(2, 'Deserts'),
+(3, 'Rice'),
+(4, 'Biryani'),
+(5, 'Curry');
 
 -- --------------------------------------------------------
 
@@ -98,15 +104,7 @@ INSERT INTO `tbl_cuisines` (`id`, `name`, `created_on`) VALUES
 (1, 'French ', '2023-12-13 09:53:27'),
 (2, 'Chinese ', '2023-12-13 09:53:27'),
 (3, 'Japanese', '0000-00-00 00:00:00'),
-(4, 'Greek', '0000-00-00 00:00:00'),
-(5, 'Spanish', '0000-00-00 00:00:00'),
-(6, 'Lebanese', '0000-00-00 00:00:00'),
-(7, 'Turkey', '0000-00-00 00:00:00'),
-(8, 'Thai', '0000-00-00 00:00:00'),
-(9, 'Mexican', '0000-00-00 00:00:00'),
 (10, 'American', '0000-00-00 00:00:00'),
-(11, 'Mediterranean', '0000-00-00 00:00:00'),
-(12, 'Caribbean', '0000-00-00 00:00:00'),
 (13, 'Italian', '0000-00-00 00:00:00'),
 (14, 'Indian', '2023-12-13 09:58:36');
 
@@ -130,7 +128,15 @@ CREATE TABLE `tbl_events` (
 
 INSERT INTO `tbl_events` (`event_id`, `event_name`, `event_date`, `event_image`, `event_type`) VALUES
 (1, 'Diwali', '2024-02-23', 'uploads/20240201225533.png', 'Event'),
-(2, 'Birthday Party', '0000-00-00', 'uploads/20240201225855.png', 'Special');
+(2, 'Birthday Party', '0000-00-00', 'uploads/20240201225855.png', 'Special'),
+(3, 'Holi', '2024-03-25', 'uploads/20240312065552.png', 'Event'),
+(4, 'Ugadi', '2024-04-09', 'uploads/20240312065705.png', 'Event'),
+(5, 'Ramanavami', '2024-04-19', 'uploads/20240312065732.png', 'Event'),
+(6, 'Ganesh Charturthi', '2024-09-07', 'uploads/20240312065928.png', 'Event'),
+(7, 'Anniversary', '0000-00-00', 'uploads/20240312070045.png', 'Special'),
+(8, 'Inviting Guests', '0000-00-00', 'uploads/20240312070119.png', 'Special'),
+(9, 'Family Meetup', '0000-00-00', 'uploads/20240312070135.png', 'Special'),
+(10, 'Diwali', '2024-11-01', 'uploads/20240327043943.png', 'Event');
 
 -- --------------------------------------------------------
 
@@ -150,6 +156,8 @@ CREATE TABLE `tbl_login` (
   `last_name` varchar(250) NOT NULL,
   `address_line_1` varchar(250) NOT NULL,
   `address_line_2` varchar(250) NOT NULL,
+  `exp` varchar(200) NOT NULL,
+  `city` varchar(100) NOT NULL,
   `postcode` varchar(100) NOT NULL,
   `state` varchar(100) NOT NULL,
   `country` varchar(250) NOT NULL
@@ -159,10 +167,15 @@ CREATE TABLE `tbl_login` (
 -- Dumping data for table `tbl_login`
 --
 
-INSERT INTO `tbl_login` (`id`, `username`, `password`, `email`, `image`, `user_type`, `mobile_number`, `first_name`, `last_name`, `address_line_1`, `address_line_2`, `postcode`, `state`, `country`) VALUES
-(1, 'admin', '12345', 'admin@admin.com', '', 'Admin', '9392522044', '', '', '', '', '', '', ''),
-(2, 'ganesh', '12345', 'ganeshvunnam8@gmail.com', 'uploads/20240202102147.png', 'chef', '9392522044', '', '', '', '', '', '', ''),
-(3, 'Test', '1234', 'test@login.com', '', 'user', '123456789', 'Test', 'User', '', '', '', '', '');
+INSERT INTO `tbl_login` (`id`, `username`, `password`, `email`, `image`, `user_type`, `mobile_number`, `first_name`, `last_name`, `address_line_1`, `address_line_2`, `exp`, `city`, `postcode`, `state`, `country`) VALUES
+(1, 'admin', '12345', 'admin@admin.com', '', 'Admin', '9392522044', '', '', '', '', '0', '', '', '', ''),
+(2, 'ganesh', '12345', 'ganeshvunnam8@gmail.com', '', 'chef', '9392522044', 'GANESH', 'VUNNAM', '5-78, Chowtapalem (vlg), Prakasam (dist)', '', '5 Years', 'Chennai', '523109', 'Andhra Pradesh', 'India'),
+(3, 'Test', '1234', 'test@login.com', '', 'user', '123456789', 'Test', 'User', '', '', '0', '', '', '', ''),
+(4, 'Ganesh', '12345', 'ganeshvunnam8@gmail.com', 'uploads/20240327065529.png', 'user', '9392522044', '', '', '', '', '0', '', '', '', ''),
+(5, 'len', '1234', 'len@gmail.com', '', 'user', '765432345654', '', '', '', '', '0', '', '', '', ''),
+(6, 'vamsi', '12345', 'vamsi123@gmail.com', '', 'chef', '9177053680', '', '', '', '', '0', '', '', '', ''),
+(7, 'Naveen', '12345', 'naveen123@gmail.com', 'uploads/20240327065529.png', 'chef', '9949909092', '', '', '', '', '0', '', '', '', ''),
+(8, 'koti', '12345', 'koti123@gmail.com', '', 'chef', '9177053681', '', '', '', '', '0', '', '', '', '');
 
 -- --------------------------------------------------------
 
@@ -186,7 +199,22 @@ INSERT INTO `tbl_menu` (`id`, `cid`, `category`, `dish`, `price`) VALUES
 (1, 14, 'Sandwich', 'Plain Sandwich', 175),
 (2, 14, 'Sandwich', 'Grilled Sandwich', 175),
 (3, 14, 'Sandwich', 'Club Sandwich', 175),
-(4, 14, 'Deserts', 'Kulfi', 60);
+(4, 14, 'Deserts', 'Kulfi', 60),
+(5, 14, 'Rice', 'Steam Rice', 80),
+(6, 14, 'Rice', 'Sambar Rice', 120),
+(7, 14, 'Rice', 'Curd Rice', 110),
+(8, 14, 'Rice', 'Lemon Rice', 110),
+(9, 0, 'Biryani', 'Hyderabadi Biryani', 300),
+(10, 14, 'Biryani', 'Hyderabadi Biryani', 300),
+(11, 14, 'Biryani', 'Chicken Biryani', 250),
+(12, 14, 'Biryani', 'Mutton Biryani', 500),
+(13, 14, 'Curry', 'Chicken Curry', 200),
+(14, 14, 'Curry', 'Mutton Curry', 350),
+(15, 14, 'Curry', 'Fish Curry', 350),
+(16, 14, 'Curry', 'Sambar', 100),
+(17, 14, 'Curry', 'Rasam', 100),
+(18, 14, 'Deserts', 'Bread Halwa', 100),
+(19, 14, 'Deserts', 'Gulab Jamun', 80);
 
 -- --------------------------------------------------------
 
@@ -259,13 +287,13 @@ ALTER TABLE `tbl_profile`
 -- AUTO_INCREMENT for table `tbl_bookings`
 --
 ALTER TABLE `tbl_bookings`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 
 --
 -- AUTO_INCREMENT for table `tbl_category`
 --
 ALTER TABLE `tbl_category`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `tbl_cuisines`
@@ -277,19 +305,19 @@ ALTER TABLE `tbl_cuisines`
 -- AUTO_INCREMENT for table `tbl_events`
 --
 ALTER TABLE `tbl_events`
-  MODIFY `event_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `event_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `tbl_login`
 --
 ALTER TABLE `tbl_login`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `tbl_menu`
 --
 ALTER TABLE `tbl_menu`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- AUTO_INCREMENT for table `tbl_profile`
